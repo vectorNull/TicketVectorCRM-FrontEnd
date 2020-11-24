2ef938ce131213b18e98d7e230ec9e2220c997e0
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2020-11-23 17:01:37 -0800

    updates styling on login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-export const Login = () => {
+export const Login = ({ handleOnChange, email, password }) => {
 	return (
 		<Container>
 			<Row>
 				<Col>
-					<h1>Client Login</h1>
+					<h1 className='text-info text-center'>Client Login</h1>
 					<hr />
 					<Form>
 						<Form.Group>
 							<Form.Label>Email Address</Form.Label>
-							<Form.Control type='email' name='email' placeholder='Enter Email' required />
+							<Form.Control
+								type='email'
+								name='email'
+								value={email}
+								onChange={handleOnChange}
+								placeholder='Enter Email'
+								required
+							/>
 						</Form.Group>
-                        <Form.Group>
+						<Form.Group>
 							<Form.Label>Password</Form.Label>
-							<Form.Control type='password' name='password' placeholder='Enter password' required />
+							<Form.Control
+								type='password'
+								name='password'
+								value={password}
+								onChange={handleOnChange}
+								placeholder='Enter password'
+								required
+							/>
 						</Form.Group>
-                        <Button type="submit">Login</Button>
+						<Button type='submit'>Login</Button>
 					</Form>
+					<hr />
 				</Col>
 			</Row>
+			<Col>
+				<a href='#!'>Forgot Password?</a>
+			</Col>
 		</Container>
 	);
 };
--- src/pages/entry/EntryPage.jsx
+++ src/pages/entry/EntryPage.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './EntryPage.css';
 import { Jumbotron } from 'react-bootstrap';
 import { Login } from '../../components/login/Login';
 
 export const EntryPage = () => {
+	const [ email, setemail ] = useState('');
+	const [ password, setpassword ] = useState('');
+
+	const handleOnChange = (event) => {
+		const { name, value } = event.target;
+		console.log('name: ', name);
+        console.log('value: ', value);
+
+	};
+
 	return (
 		<div className='entry-page bg-info'>
-			<Jumbotron>
-                <Login />
-            </Jumbotron>
+			<Jumbotron className='form-box'>
+				<Login handleOnChange={() => handleOnChange} email={email} password={password} />
+			</Jumbotron>
 		</div>
 	);
 };
